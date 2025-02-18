3504989bfce255e7390f9dd02c5cfd45153bfb70
Tanishka4202 <jaintanishka6263@gmail.com>
2025-02-18 15:34:12 +0530

    testing deployment

---
+++ script.js
@@ -0,0 +1,73 @@
+let insta = document.querySelectorAll('.insta');
+let linkedin = document.querySelectorAll('.linkedin');
+let github = document.querySelectorAll('.github');
+let email = document.querySelectorAll('.email');
+
+insta.forEach(element => {
+    element.addEventListener("click", ()=>{
+        window.open('https://www.instagram.com/tanishka376?igsh=NXFsN21oMGE2d2U=', '_blank')
+    })
+});
+
+linkedin.forEach(e=>{
+    e.addEventListener("click", ()=>{
+    window.open('https://www.linkedin.com/in/tanishka-jain-7121b5332?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app')
+})
+})
+
+github.forEach(e=>{
+    e.addEventListener("click", ()=>{
+        window.open('https://github.com/Tanishka4202')
+    })
+})
+ 
+email.forEach(e=>{
+    e.addEventListener("click", ()=>{
+        window.open('jaintanishka6263@gmail.com')
+    })
+    
+})
+
+document.getElementById("melodex").addEventListener("click", ()=>{
+    window.open('jaintanishka6263@gmail.com')
+})
+
+
+document.getElementById("netflix").addEventListener("click", ()=>{
+    window.open(' https://tanishka4202.github.io/Melodex/')
+})
+
+
+document.getElementById("youtubeUI").addEventListener("click", ()=>{
+    window.open('jaintanishka6263@gmail.com')
+})
+
+
+document.getElementById("palin").addEventListener("click", ()=>{
+    window.open('jaintanishka6263@gmail.com')
+})
+
+
+document.getElementById("roman").addEventListener("click", ()=>{
+    window.open('jaintanishka6263@gmail.com')
+})
+
+
+document.getElementById("validator").addEventListener("click", ()=>{
+    window.open('jaintanishka6263@gmail.com')
+})
+
+
+document.getElementById("portfolioUI").addEventListener("click", ()=>{
+    window.open('jaintanishka6263@gmail.com')
+})
+
+
+document.getElementById("productlanding").addEventListener("click", ()=>{
+    window.open('jaintanishka6263@gmail.com')
+})
+
+
+document.getElementById("tribute").addEventListener("click", ()=>{
+    window.open('jaintanishka6263@gmail.com')
+})
